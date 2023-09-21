9d741cdd22feb2a43115b179e2a2cf074198b60d
XiaY1123 <xy873178674@gmail.com>
2023-09-21 12:17:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup >
+import SonCom from './views/SonCom.vue'
+import {ref} from 'vue'
+const count = ref(0)
+const changeCount = ()=>{
+  count.value++
+}
+const getMessage =(msg)=>{
+  console.log(msg);
+}
+</script>
+
+<template>
+  <div class="father">
+    <h2>我是父组件App</h2>
+    <h3>count:{{ count }}</h3>
+    <button @click="changeCount">点我count+1</button>
+    <SonCom :count="count" @getMessage="getMessage" message="fathre - message"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
